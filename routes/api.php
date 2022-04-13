20d96dbdbb3bb8c67a63fb95602dfc072c7f33ec
Affan Ali <affanali577@gmail.com>
2022-04-13 13:36:45 +0500

    api routes setup

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Contact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,28 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('contacts' , function () {
+    return Contact::latest()->orderBy('created_at', 'desc')->get();
+});
+
+Route::get('contact/{id}', function ($id) {
+    return Contact::findOrFail($id);
+});
+
+Route::post('contact/store', function (Request $request) {
+    return Contact::create($request->all());
+});
+
+Route::patch('contact/{id}', function (Request $request, $id) {
+    $contact = Contact::findOrFail($id);
+    $contact->update($request->all());
+    return $contact;
+});
+
+Route::delete('contact/{id}', function ($id) {
+    return Contact::findOrFail($id)->delete();
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
